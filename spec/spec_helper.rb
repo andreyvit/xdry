4e8fa8e4b7e5d0fd3704ca34a9fdd82aa6edf15a
Andrey Tarantsov <andreyvit@gmail.com>
2010-08-29 10:48:31 +0700

    Extract common test code into helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,33 @@ require 'xdry'
 require 'spec'
 require 'spec/autorun'
 
+def xdry content
+  verbose = ((ENV['VERBOSE'] || '0').to_i != 0)
+
+  first_line_indent = content.split("\n", 2).first.gsub(/\S.*$/, '')
+  deleted_indent    = first_line_indent.sub(/\s\s$/, '-( |$)')
+  added_indent      = first_line_indent.sub(/\s\s$/, "\\\\+( |$)")
+
+  orig_content = content.lines.select { |l| l =~ /^(#{first_line_indent}|#{deleted_indent})|^$/ }.join("")
+  new_content  = content.lines.select { |l| l =~ /^(#{first_line_indent}|#{added_indent})|^$/ }.join("")
+
+  orig_content = orig_content.gsub(/^(#{first_line_indent}|#{deleted_indent})/, '')
+  new_content  = new_content.gsub(/^(#{first_line_indent}|#{added_indent})/, '')
+
+  if verbose
+    puts "-" * 40
+    puts orig_content
+    puts "-" * 40
+    puts new_content
+    puts "-" * 40
+  end
+
+  result = XDry.test_run({'main.m' => orig_content}, verbose)['main.m']
+  result = orig_content if result.nil?
+
+  result.should == new_content
+end
+
 Spec::Runner.configure do |config|
-  
+
 end
--- spec/synthesize_spec.rb
+++ spec/synthesize_spec.rb
@@ -1,33 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Xdry" do
-
-  def xdry content
-    verbose = ((ENV['VERBOSE'] || '0').to_i != 0)
-
-    first_line_indent = content.split("\n", 2).first.gsub(/\S.*$/, '')
-    deleted_indent    = first_line_indent.sub(/\s\s$/, '-( |$)')
-    added_indent      = first_line_indent.sub(/\s\s$/, "\\\\+( |$)")
-
-    orig_content = content.lines.select { |l| l =~ /^(#{first_line_indent}|#{deleted_indent})|^$/ }.join("")
-    new_content  = content.lines.select { |l| l =~ /^(#{first_line_indent}|#{added_indent})|^$/ }.join("")
-
-    orig_content = orig_content.gsub(/^(#{first_line_indent}|#{deleted_indent})/, '')
-    new_content  = new_content.gsub(/^(#{first_line_indent}|#{added_indent})/, '')
-
-    if verbose
-      puts "-" * 40
-      puts orig_content
-      puts "-" * 40
-      puts new_content
-      puts "-" * 40
-    end
-
-    result = XDry.test_run({'main.m' => orig_content}, verbose)['main.m']
-    result = orig_content if result.nil?
-
-    result.should == new_content
-  end
+describe "@synthesize support" do
 
   it "should handle empty input" do
     xdry("  ")
