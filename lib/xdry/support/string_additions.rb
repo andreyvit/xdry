f587100539649a5b99831e830e4ada4f3efe266a
Andrey Tarantsov <andreyvit@gmail.com>
2011-03-20 06:58:50 +0600

    Whitespace fix

--- lib/xdry/support/string_additions.rb
+++ lib/xdry/support/string_additions.rb
@@ -6,14 +6,14 @@ module XDry
     def blank?
       self =~ /^\s*$/
     end
-    
+
     def capitalized_identifier
       case self
         when 'id', 'uid' then upcase
         else self[0..0].upcase + self[1..-1]
       end
     end
-    
+
     def prefixed_as_arg_name
       prefix = case self when /^[aeiou]/ then 'an' else 'a' end
 
